6dd4e2474ce158b2c2f6dd61fb3e253352c81a6b
Brian Quinn <briandquinn@gmail.com>
2010-09-06 14:50:42 +0100

    Quick and dirty Rails 3 routing fix

--- init.rb
+++ init.rb
@@ -1,1 +1,7 @@
-ActionController::Routing::RouteSet::Mapper.send(:include, MongoDBLogging::RoutingExtensions)
+klass = if ActionPack::VERSION::MAJOR >= 3
+  ActionDispatch::Routing::DeprecatedMapper
+else
+  ActionController::Routing::RouteSet::Mapper
+end
+
+klass.send(:include, MongoDBLogging::RoutingExtensions)
